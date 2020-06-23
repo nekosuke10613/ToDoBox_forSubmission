1d763cfcbbbcc19f1d40487760670d3060ef2e55
nekosuke10613 <nekosuke10613@gmail.com>
2020-06-23 16:41:34 +0900

    ボタン機能途中

---
+++ GridTaskApp/Assets/MyApp/Scripts/AppButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//*-- 　ボタンの共通機能  --*
+public class AppButton : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Init()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //押下時
+
+    //クリックした時
+}
---
+++ GridTaskApp/Assets/MyApp/Scripts/HomeManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum HomePage
+{
+    None = 0,
+    List = 1,   //一覧
+    Add = 2,    //登録
+    Option = 3, //
+}
+public class HomeManager : MonoBehaviour
+{
+    [SerializeField,Header("各ページのプレハブを生成する親オブジェクト")]
+    GameObject m_pagesParent;
+
+    [SerializeField,Header("各ページプレハブ")]
+    GameObject[] m_pagePrefubs = null;
+
+    //現在のページ名
+    HomePage m_currentPage = HomePage.List;
+
+    //TODO 後で消す　テスト用
+    private void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        //全てのページを非アクティブにしてからオンにする
+        m_currentPage = HomePage.List;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick(int nextPage)
+    {
+        //同じページだったらreturn
+        if (nextPage == (int)m_currentPage)
+            return;
+
+
+    }
+}
