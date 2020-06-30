b1cb281d8c259a251045979351c0ce243fe5f56a
nekosuke10613 <nekosuke10613@gmail.com>
2020-06-30 19:35:10 +0900

    シングルトンものびへーびあー追加

--- GridTaskApp/Assets/MyApp/Scripts/Task/DataTaskManager.cs
+++ GridTaskApp/Assets/MyApp/Scripts/Task/DataTaskManager.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-struct Task
+public class Task
 {
-    int id;//タスク自体の識別ID
-    int houseID; //登録したタスク表の場所
-    string name;　//タスクの名前
-    string description;　//タスクの詳細説明
-    bool isFinish;　//このタスクは完了したものか
+    public int ID;//タスク自体の識別ID
+    public int HouseID; //登録したタスク表の場所
+    public string Name;　//タスクの名前
+    public string Description;　//タスクの詳細説明
+    public bool IsFinish;　//このタスクは完了したものか
+
+
 }
 //タスクデータの管理クラス　シングルトンにしたい
-public class DataTaskManager : MonoBehaviour
+public class DataTaskManager : SingletonMonoBehaviour<DataTaskManager>
 {
-    public readonly Vector2 TaskInitPos = new Vector2(-300, 400);
+    public readonly Vector2 TaskInitPos = new Vector2(-300, 0);
     public readonly Vector2 TaskSpace = new Vector2(300, 200);
 
     public int AllTaskNum = 0;
@@ -30,14 +32,27 @@ public class DataTaskManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
+    public void Init()
+    {
+        //最初期は3*5の空白が設定してある
+        //TODO: Instanciateしたくない
+        for(int i = 0; i < 5; i++)
+            CreateThreeEmpty();
 
-    // Update is called once per frame
-    void Update()
+        // データ保存してある状態なら各方眼に情報を入れる
+    }
+    
+    /// <summary>
+    /// タスクの情報を作成して既存のボックスに代入
+    /// </summary>
+    public void CreateInfo(Task task)
     {
         
     }
+    //単体方眼をクリエイト
+    //TODO : (指定した空白に情報を詰める処理にする)
     public void CreateTask(string name)
     {
         var task = Instantiate(m_singleTask,m_parent);
@@ -45,5 +60,22 @@ public class DataTaskManager : MonoBehaviour
         AllTaskNum++;
         task.SetPosition(TaskInitPos,TaskSpace,AllTaskNum-1);
         
+    }
+    //1行３つのの空白を追加する
+    public void CreateThreeEmpty()
+    {
+        CreateTask("");
+        CreateTask("");
+        CreateTask("");
+    }
+
+    /// <summary>
+    /// タスク行の高さを取得
+    /// </summary>
+    public float TaskHeight()
+    {
+        int s = AllTaskNum / 3;
+        float height = s *  TaskSpace.y;
+        return height;
     }
 }
